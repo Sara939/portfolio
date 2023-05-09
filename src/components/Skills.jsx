044368f4488e9cf9060c40abde5c3c5a2f85f498
sara-likaount <saralikaunt@gmail.com>
2023-05-09 22:36:10 +0300

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,8 +2,54 @@ import React from 'react'
 
 function Skills() {
   return (
-    <div>Skills</div>
-  )
-}
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+          <div className=' w-full flex justify-center items-center flex-col mb-7'>
+              <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-center '>Skills</p>
+              <p className='py-4 text-2xl'>I enjoy and good at self-learning, so Here's a list of technologies I've worked with</p>
+          </div> 
+          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>HTML-CSS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>JAVA SCRIPT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>TYPE SCRIPT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>REACT, REACT HOOKS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>NODE JS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>EXPRESS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>MONGO DB</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>WEB API</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>BOOTSTRAP</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>MUI</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'> TAILWINDCSS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>GIT, GITHUB</p>
+              </div>
+          </div>
+      </div>
+    </div>
+  );
+};
 
 export default Skills
