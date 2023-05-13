6dc36854224077ba9cbd4ef10bd56b1b5a87d7ca
sara-likaount <saralikaunt@gmail.com>
2023-05-13 21:50:46 +0300

    /

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import techshare from '../assets/techshare.png';
+import techshare from '../assets/techshare.png'
 import getinspierd from '../assets/getinspierd.png'
-import profilepage from '../assets/profilepage.png'
 import dreamytrips from '../assets/dreamytrips.png'
-import cellra from '../assets/cellra.png'
 
 function Works() {
   return (
@@ -66,54 +64,7 @@ function Works() {
               </div>
             </div>
           </div>
-          <div
-            style={{ backgroundImage: `url(${cellra})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
-              <span className=' text-lg font-bold text-white tracking-wider'>
-                cellra- cellular radiation effects Application
-              </span>
-              <p className='text-center'>A CBT web application built with Java script and Web api, Bootstrap</p>
-              <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${techshare})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
-              <span className=' text-lg font-bold text-white tracking-wider'>
-                Plants Order Application
-              </span>
-              <p className='text-center'>A CBT web application built with React </p>
-              <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
+          
           <div
             style={{ backgroundImage: `url(${dreamytrips})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
@@ -138,31 +89,6 @@ function Works() {
               </div>
             </div>
           </div>
-          <div
-            style={{ backgroundImage: `url(${profilepage})` }}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          >
-            {/* Hover Effects */}
-            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
-              <span className=' text-lg font-bold text-white tracking-wider'>
-              Profile page- My first Project
-              </span>
-              <p className='text-center'>A profile page built with HTML And css</p>
-              <div className='pt-8 text-center'>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Demo
-                  </button>
-                </a>
-                <a href='/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          
         </div>
       </div>
     </div>
